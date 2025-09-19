7a739455d5ffdf7be2a3e350ea3057532a4a9ba6
Gaurakh <68327125+GAURAKH-97@users.noreply.github.com>
2025-09-19 17:37:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,29 @@ app.use(express.json())
 
 const PORT = process.env.PORT || 3000;
 app.get('/',(req,res)=>{
-    res.send("<h1>welcome to home page</h1><p>jjj</p>")
+    const data={
+        "activities":{
+        "today_timespent":190,
+        "today_focus":120,
+        "distraction_website":{
+            "Tiktok":8,
+            "Youtube":2,
+            "Instagram":3
+        },
+        "past7days":{
+            "19 sept":{distraction:3,focus:1},
+            "18 sept":{distraction:6,focus:6},
+            "17 sept":{distraction:7,focus:13},
+            "16 sept":{distraction:4,focus:180},
+            "15 sept":{distraction:2,focus:190},
+            "14 sept":{distraction:9,focus:3},
+            "13 sept":{distraction:7,focus:6}
+        }
+    },
+        "trackingEnabled":true
+    }
+    
+    res.json(data)
     
 })
 app.set("view engine", "ejs");
